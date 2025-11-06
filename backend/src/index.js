303decc53727d8582b28d6b418da5ba0c66ebe7e
Piyush varma <varmapiyush864@gmail.com>
2025-11-06 20:26:09 +0530

    fixed errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,14 +29,10 @@ app.use('/api/messages',messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
-     app.get("/*", (req, res) => {
+     app.get(/.*/, (req, res) => {
      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
-app.use('/',(req,res)=>{
-    res.send("Chat app");
-})
 
 server.listen(PORT, () => {
     console.log("✅ Server is running on http://localhost:"+PORT);
